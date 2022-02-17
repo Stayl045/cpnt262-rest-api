47dc42a23cee06efb4ff6b04631be717f4c75509
Stephan Taylor <stephantaylor21@hotmail.com>
2022-02-17 10:18:05 -0700

    added html and js content

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const fetchData = async function (url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+
+    console.log(data.data[8].films);
+
+    const filmsOutput = `<ul><li>${data.data[8].films.join(
+      "</li><li>"
+    )}</li></ul>`;
+
+    const output = `
+        <h1>${data.data[8].name}</h1>
+        <img src="${data.data[8].imageUrl}" alt="Image of ${data.data[8].name}">
+        <p>
+          <a href="${data.data[8].url}">Raw Data</a>
+        </p>
+        <p>Films</p>
+        ${filmsOutput}
+      `;
+
+    document.querySelector(".content").innerHTML = output;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const endpoint = "http://api.disneyapi.dev/characters";
+
+fetchData(endpoint);
