1e36b5a1665c08c0e464bf4814ff32e1e38ebad3
Stephan Taylor <stephantaylor21@hotmail.com>
2022-02-22 10:00:40 -0700

    added styling, js, and selectors

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 ## Code Journal Plan
 
+### Goal
+
+To create an interactive API that cycles through the API on a click of a button
+
 1. Copy finished Disney API Boiler
 2. Find a API I want to use
 3. Copy API to the link
+
+### Struggles
+
+- I at first struggled with getting the APIs to work. So I sought the help of my brother. It turns out that I was choosing APIs that were much to complex for my current level. He suggested I choose something easier...which lead me to the Digimon API
+  - He showed me a different REST API method from what you showed that class. Which was great and taught me other methods of using `FetchData` functions.
+    - This worked, but it printed out the entire page of objects rather then a single one.
+  - I instead went back to my original JS layout, but played around with it a bunch to function the way I wanted with great success I might add!
+    - You'll notice the previous JS commented out below the new one.
+- I was able to make the `btn` selector work successfully to cycle through each objects all on my own. I was quite proud of myself to do it on my own.
+
+### Attributions
+
+- My brother Adrian Taylor
+
+- [The Coding Train](https://www.youtube.com/watch?v=tc8DU14qX6I&t=640s)
---
+++ assets/js/main.js
@@ -0,0 +1,104 @@
+const btn = document.querySelector(".btn");
+
+const randomIndex = (length) => {
+  return Math.floor(Math.random() * length);
+};
+
+const fetchData = async function (url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    const characters = data[randomIndex(data.length)];
+
+    console.log(characters);
+
+    const output = `
+    <h1>${characters.name}</h1>
+    <img src= "${characters.img}">
+    <p>${characters.level}</p>
+
+    `;
+
+    document.querySelector(".content").innerHTML = output;
+  } catch (error) {
+    console.error("No Error is Present");
+  }
+};
+
+const submitHandler = (event) => {
+  event.preventDefault();
+};
+
+btn.addEventListener("click", function () {
+  fetchData(endpoint);
+});
+
+const endpoint = "https://digimon-api.vercel.app/api/digimon";
+fetchData(endpoint);
+
+// const click = function () {
+//   fetchData(endpoint);
+// };
+
+// const btn = document.querySelector(".btn");
+// // const txt = document.querySelector(".txt");
+// // const submit = document.querySelector(".submit");
+// const content = document.querySelector(".content");
+
+// const randomIndex = (length) => {
+//   return Math.floor(Math.random() * length);
+// };
+
+// const fetchData = async () => {
+//   try {
+//     const response = await fetch("https://digimon-api.vercel.app/api/digimon");
+//     if (!response.ok) {
+//       throw new Error(response.statusText);
+//     }
+//     const data = await response.json();
+//     // console.log(data.data[randomIndex(data.data.length)]);
+
+//     // console.log(data[randomIndex(data.length)]);
+//     // console.log(characters);
+//     const html = data
+//       .map(
+//         (el) => `
+//     <h1>${el.name}</h1>
+//     <img src= "${el.img}">
+//     <p>${el.level}</p>
+
+//     `
+//       )
+//       .join("");
+
+//     content.insertAdjacentHTML("afterbegin", html);
+
+//     // document.querySelector(".content").innerHTML = output;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// // const endpoint = "https://digimon-api.vercel.app/api/digimon";
+
+// // fetchData(endpoint);
+
+// // btn.addEventEventListener("click", function (e) {
+// //   e.preventDefault();
+// //   fetchData();
+// // });
+
+// // btn.addEventListener("click", fetchData);
+
+// // fetchData();
+
+// const submitHandler = (event) => {
+//   event.preventDefault();
+
+//   btn.addEventListener("click", submitHandler);
+// };
+
+// fetchData();
