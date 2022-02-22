a995f1bb9535b174d956d1edce33ffc8d3c59300
Stephan Taylor <stephantaylor21@hotmail.com>
2022-02-21 20:49:30 -0700

    recent commit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
+const btn = document.querySelector(".btn");
+// const txt = document.querySelector(".txt");
+// const submit = document.querySelector(".submit");
+const content = document.querySelector(".content");
+
 const randomIndex = (length) => {
   return Math.floor(Math.random() * length);
 };
 
-const fetchData = async function (url) {
+const fetchData = async function () {
   try {
-    const response = await fetch(url);
+    const response = await fetch("https://digimon-api.vercel.app/api/digimon/");
     if (!response.ok) {
       throw new Error(response.statusText);
     }
     const data = await response.json();
-    const characters = data[randomIndex(data.length)];
+    // const characters = data[randomIndex(data.length)];
 
-    console.log(characters);
-    const output = `
-    <h1>${characters.name}</h1>
+    // console.log(characters);
+    const html = data
+      .map(
+        (el) =>
+          `
+    <h1>${el.name}</h1>
+    <img src= "${el.name}">
     
-    `;
+    `
+      )
+      .join("");
 
-    document.querySelector(".content").innerHTML = output;
+    content.insertAdjacentElement("afterbegin", html);
+
+    // document.querySelector(".content").innerHTML = output;
   } catch (error) {
     console.error(error);
   }
 };
 
-const endpoint = "https://anapioficeandfire.com/api/characters/583";
+// const endpoint = "https://digimon-api.vercel.app/api/digimon";
 
-fetchData(endpoint);
+// fetchData(endpoint);
+
+btn.addEventEventListener("click", function (e) {
+  e.preventDefault();
+  fetchData();
+});
